4ab3e01d17da39aa5ed3f9bb8416c0a0e3bc9565
MorpheusByte <tahasaidcakmak@icloud.com>
2025-05-12 21:16:43 +0300

    1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container ,Image } from 'react-bootstrap'
+import logo from "../../assets/logo.png"
 
 const Header = () => {
   return (
     <Container>
+        <Image src={logo} width="200px"/>
         <h1 className='my-2 text-uppercase title'>Football Legends</h1>
     </Container>
   )
